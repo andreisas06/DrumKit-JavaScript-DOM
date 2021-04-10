de4df983f7e36e47acb54e9d61e9398c5dbc7616
Andrei Sas <andreisas007@gmail.com>
2021-04-10 11:40:31 +0300

    Added event listeners to buttons

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function handleClick(){
-    alert("I got clicked");
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+		alert("I got clicked");
+	});
 }
